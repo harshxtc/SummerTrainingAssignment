59ae63c12189280ff9040d0a5c051729643b52a8
Harsh Jadaun <harshxtc@gmail.com>
2026-06-20 11:18:37 +0530

    activity

---
+++ 1stActivity/prob1.cpp
@@ -0,0 +1,14 @@
+// palindrome fxn
+#include<iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+int main(){
+    string str1,str2;
+    cin>>str1>>str2;
+    reverse(str1.begin(),str1.end());
+    if(str1==str2){
+        cout<<"It is palindrome...";
+    }
+    return 0;
+}
